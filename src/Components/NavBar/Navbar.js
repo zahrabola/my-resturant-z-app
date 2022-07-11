7d9219a4d8fd6adc657b8a4fbf467288a1fee43c
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-07-11 15:14:46 +0100

    Update Navbar.css

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import image from "../../Constants/images"
 import "./Navbar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
 const Navbar = () => {
 
@@ -26,6 +28,40 @@ return (
         <a href="#contact">Contact</a>
       </li>
     </ul>
+    <div className="navbarlogin">
+      <a href="#login" className="text">
+        Log In / Registration
+      </a>
+      <div />
+      <a href="/" className="text">
+        Book Table
+      </a>
+    </div>
+    <div className="text"> Hello </div>
+    <div className="navbarsmallscreen">
+      <GiHamburgerMenu color="#fff" fontSize={27} />
+
+      <div className="navbarsmallscreenOverlay flex__center slide-bottom">
+        <MdOutlineRestaurantMenu fontSize={27} className="overlayclose" />
+        <ul className="navbarsmallscreen-links">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#menu">Menu</a>
+          </li>
+          <li>
+            <a href="#awards">Awards</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
 );
 }
