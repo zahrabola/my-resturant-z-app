c0b102d5b215b27972a2ee8d967cfcf670fcb517
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-07-12 13:06:13 +0100

    Update special menu

--- src/Container/Menu/SpecialMenu.js
+++ src/Container/Menu/SpecialMenu.js
@@ -9,7 +9,15 @@ const SpecialMenu = () => {
       <div className="specialMenu flex__center sectionpadding" id="menu">
         <div className="specialMenu-title">
           <SubHeading title="Menu that fits your palatte" />
+          <h1 className="headtext">Today's Specials</h1>
         </div>
+<div className="specialMenu-menu">
+    <div className="specialMenu-wine flex__center">
+        
+    </div>
+</div>
+
+
       </div>
     );
     }
