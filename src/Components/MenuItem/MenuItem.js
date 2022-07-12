b00d07ec57be3464f2ca0abb8d87a24c118df14f
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-07-12 14:37:02 +0100

    Update Menu item

--- src/Components/MenuItem/MenuItem.js
+++ src/Components/MenuItem/MenuItem.js
@@ -2,8 +2,26 @@ import React from "react";
 import "./MenuItem.css";
 const MenuItem = ({title, price, tags}) => {
     return (
-
-    )
+      <div className="menuitem">
+        <div className="menuitem-head">
+          <div className="menuitem-name">
+            <p className="pstyle">{title}</p>
+          </div>
+          <div className="menuitemdash">
+            <div className="menuitem-price">
+              <p className="pstyle" style={{ color: "#DCCA87" }}>
+                {price}
+              </p>
+            </div>
+          </div>
+          <div className="menuitem-sub">
+            <p className="pstyle" style={{ color: "#AAAAAA" }}>
+                {tags}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 
