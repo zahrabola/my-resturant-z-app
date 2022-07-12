e01abc2362c3629533c13a6824a43128bb25282b
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-07-12 15:10:02 +0100

    Update Special menu

--- src/Container/Menu/SpecialMenu.js
+++ src/Container/Menu/SpecialMenu.js
@@ -17,32 +17,40 @@ const SpecialMenu = () => {
             <p className="specialMenu-heading">Wine and Beer</p>
             <div className="specialMenu-items">
               {Data.wines.map((wine, index) => (
-<MenuItem  key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags}/>      
+                <MenuItem
+                  key={wine.title + index}
+                  title={wine.title}
+                  price={wine.price}
+                  tags={wine.tags}
+                />
               ))}
             </div>
           </div>
 
           <div className="specialMenu-img flex__center">
-<img src={image.menu} alt="menuimg"/>
+            <img src={image.menu} alt="menuimg" />
           </div>
 
           <div className="specialMenu-cocktail flex__center">
             <p className="specialMenu-heading">Cocktails</p>
             <div className="specialMenu-items">
-{Data.cocktails.map((cocktail, index) => (
-    <p>{cocktail.title}</p>
-))}
+              {Data.cocktails.map((cocktail, index) => (
+                <MenuItem
+                  key={cocktail.title + index}
+                  title={cocktail.title}
+                  price={cocktail.price}
+                  tags={cocktail.tags}
+                />
+              ))}
             </div>
           </div>
         </div>
 
-<div style={{marginTop: 15 }}>
+        <div style={{ marginTop: 15 }}>
           <button type="button" className="custombtn">
-       View More
-      </button>
-</div>
-
-
+            View More
+          </button>
+        </div>
       </div>
     );
     }
