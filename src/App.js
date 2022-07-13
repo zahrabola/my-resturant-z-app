58ec4f2931825caf34d4be519dc96dc543136628
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-07-13 11:45:39 +0100

    Update intro

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AboutUs from './Container/AboutUs/AboutUs';
 import Header from './Container/Header/Header';
 import SpecialMenu from './Container/Menu/SpecialMenu';
 import Chef from "./Container/Chef/Chef";
+import Intro from './Container/Introduction/Intro';
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
   <AboutUs />
   <SpecialMenu />
   <Chef />
+  <Intro />
       <div>Zahra Olanrewaju</div>
     </div>
   );
--- src/Container/Introduction/Intro.js
+++ src/Container/Introduction/Intro.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Intro.css";
-//import {meal} from "../../Constants";
+import {meal} from "../../Constants";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
 
 
 
 const Intro = () => {
+const vidRef = React.useRef();
+const [videoplay, setVideoPlay] = useState (false);
+
     return (
-        <div className="videoapp">
-            hello 
+      <div className="videoapp">
+        <video
+          ref={vidRef}
+          src={meal}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+        />
+        <div className="videoOverlay flex__center">
+          <div
+            className="videoOverlay-circle flex__center"
+            onClick={() => {
+              setVideoPlay(!videoplay);
+              if (videoplay) {
+                vidRef.current.pause();
+              } else {
+                vidRef.current.play();
+              }
+            }}
+          >
+            {videoplay ? (
+              <BsPauseFill color="#fff" fontSize={90} />
+            ) : (
+              <BsFillPlayFill color="#fff" fontSize={90} />
+            )}
+          </div>
         </div>
-
-
-
-    )
+      </div>
+    );
 }
 
 export default Intro;
