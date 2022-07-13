cec3a017edd712e3bba28f5f9943331bcaae010e
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-07-13 12:05:42 +0100

    Update Laurels

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
   <SpecialMenu />
   <Chef />
   <Intro />
+  
       <div>Zahra Olanrewaju</div>
     </div>
   );
--- src/Container/Laurel/Laurels.js
+++ src/Container/Laurel/Laurels.js
@@ -1,1 +1,22 @@
+import React from "react";
+import image from "../../Constants/images";
+import { Data } from "../../Constants";
+import SubHeading from "../../Components/SubHeading/SubHeading";
+
+const Laurels = () => {
+    return (
+      <div className="appbg sectionpadding appwrapper" id="awards">
+        <div className="wrapperinfo">
+          <SubHeading title="Awards and recognition" />
+          <h1 className="headtext">Our Laurels</h1>
+        </div>
+      </div>
+    );
+}
+
+
+
+
+
+
 export default Laurels;
