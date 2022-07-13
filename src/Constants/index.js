6500bba694d882bdb6a2d455288a4b403b38791a
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-07-13 11:17:05 +0100

    Update intro

---
+++ src/Constants/index.js
@@ -0,0 +1,5 @@
+import images from "./images";
+import meal from "../Assets/meal.mp4";
+import Data from "./Data";
+
+export { images, meal, Data };
--- src/Container/Introduction/Intro.js
+++ src/Container/Introduction/Intro.js
@@ -1,1 +1,18 @@
+import React from "react";
+import "./Intro.css";
+//import {meal} from "../../Constants";
+
+
+
+const Intro = () => {
+    return (
+        <div className="videoapp">
+            hello 
+        </div>
+
+
+
+    )
+}
+
 export default Intro;
