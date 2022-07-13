880336cdc5c71990e7d08d39c9b878eadd77e41e
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-07-13 21:12:41 +0100

    Update Gallery

--- src/Container/Gallery/Gallery.js
+++ src/Container/Gallery/Gallery.js
@@ -5,27 +5,23 @@ import {
   BsArrowLeftShort,
   BsArrowRightShort,
 } from "react-icons/bs";
-//import { Data } from "../../Constants";
+
 import SubHeading from "../../Components/SubHeading/SubHeading";
 import './Gallery.css';
 const Gallery = () => {
 
+    const scrollRef = React.useRef(null);
+    const scroll = (direction) => {
+        const {current} = scrollRef;
+
+        if (direction === 'left') {
+            current.scrollLeft -=300;
+        } else {
+            current.scrollLeft += 300;
+        }
+    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
     return (
       <div className=" gallery flex__center">
         <div className="gallery-content">
@@ -41,21 +37,30 @@ const Gallery = () => {
           </button>
         </div>
         <div className="galleryimg">
-          <div className="galleryimg-container">
+          <div className="galleryimg-container" ref={scrollRef}>
             {[
               image.gallery01,
               image.gallery02,
               image.gallery03,
               image.gallery04,
             ].map((images, index) => (
-              <div className="galleryimg-card flex__center">
+              <div
+                className="galleryimg-card flex__center"
+                key={`galleryimage-${index + 1}`}
+              >
                 <img src={images} alt="galleryimage" />
               </div>
             ))}
           </div>
           <div className="galleryimg-arrow">
-            <BsArrowLeftShort className="Garrow-icon" />
-            <BsArrowRightShort className="Garrow-icon" />
+            <BsArrowLeftShort
+              className="Garrow-icon"
+              onClick={() => scroll("left")}
+            />
+            <BsArrowRightShort
+              className="Garrow-icon"
+              onClick={() => scroll("right")}
+            />
           </div>
         </div>
       </div>
