3159cf7fa1dd4a77dd9ada67a547275f3a72a715
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-07-13 19:38:43 +0100

    Update Gallery

--- src/Container/Gallery/Gallery.js
+++ src/Container/Gallery/Gallery.js
@@ -1,1 +1,16 @@
+import React from "react";
+import image from "../../Constants/images";
+import { Data } from "../../Constants";
+import SubHeading from "../../Components/SubHeading/SubHeading";
+import './Gallery.css';
+const Gallery = () => {
+    return (
+      <div className=" gallery flex__center">
+        <div className="gallery-content">
+          <SubHeading title="Instagram" />
+        </div>
+      </div>
+    );
+}
+
 export default Gallery;
