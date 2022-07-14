26060b6c527c83c5fb357cfaa81ed0a2ecf6c356
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-07-14 14:56:34 +0100

    Update Newsletter

--- src/Components/Footer/Newsletter.js
+++ src/Components/Footer/Newsletter.js
@@ -1,1 +1,24 @@
+import React from "react";
+import "./Newsletter.css";
+import SubHeading from "../../Components/SubHeading/SubHeading";
+
+
+const Newsletter = () => {
+    return (
+      <div className="newsletter">
+        <div className="news-heading">
+          <SubHeading title="NewsLetter " />
+          <h1 className="headtext">Subscribe To Our Newsletter</h1>
+          <p className="pstyle">And never miss latest Updates!</p>
+        </div>
+
+        <div className="news-input flex__center">
+          <input type="email" placeholder="Enter your email address" />
+          <button type="button" className="custombtn">
+          Subscribe
+          </button>
+        </div>
+      </div>
+    );
+}
 export default Newsletter;
